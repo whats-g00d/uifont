a0c663270413181acbaf0856b609c0d75fc8079d
Siva Dhayalan <dsivasuthan@gmail.com>
2020-05-18 11:31:31 -0400

    v1.4.1

--- foo.js
+++ foo.js
@@ -2,63 +2,11 @@
 var fontname;
 
 chrome.extension.sendRequest({localstorage: "fontname"}, function(response) {
-	fontname=response.fontname;
 	
-	//alert ("fontname is " + fontname);
-
-
-var eType = new Array("body","a","table","tr","td","div","h1","h2","h3","h4","h5","h6","button","input","textarea","select","span","font","select","form","p","li","ul","ol","frame");
-
-// This is the _original_ version of the extension 
-// It only works run_at is document_idle or document_end
-// doesn't work in frames on AJAX sites
-// page comes up in original font first
-
-/*
-var obj ;
-	for(var i=0;i<eType.length;++i){
-		obj = document.getElementsByTagName(eType[i]);
-			for(var j=0;j<obj.length;++j){
-				obj[j].style.fontFamily = fontname;
-			}
-		obj = null;
-	}
-*/
-
-//This is new version
-//This should work on frames and AJAX sites
-//Page comes up in new font
-//Note: Don't just add stuff to innerHTML - it doesn't work
-//  must use proper appendChild() function
-
-
-var divNode = document.createElement("div");
-divNode.innerHTML = '<style>*{font-family:' + fontname + ',sans-serif!important;}</style>';
-var head = document.getElementsByTagName('head')[0];
-head.appendChild(divNode);
-
-
-// various tries to get CSS applied to 
-// other frames where it wasn't working
-// requires modifying manifest and adding jquery file
-/*
-for(var i=0;i<eType.length;++i){
-    $(eType[i]).each(function(){
-      $(this).css('fontFamily',fontname);
-    });
-}
-
-/*
-var frames=document.getElementsByTagName("iframe");
-for (var f=0,max=frames.length;f<max;f++) {
-  $("#" + f).contents().find("HEAD").append(divNode);
-  for(var i=0;i<eType.length;++i){
-    $("#" + f).$(eType[i]).each(function(){
-      $(this).css('fontFamily','Ubuntu');
-    });
-  }
-}
-*/
-
-}
-);
+	fontname=response.fontname;
+	var divNode = document.createElement("div");
+	divNode.innerHTML = '<style>*{font-family:' + fontname + ',sans-serif!important;}</style>';
+	var head = document.getElementsByTagName('head')[0];
+	head.appendChild(divNode);
+	
+});
--- scripts.js
+++ scripts.js
@@ -1,57 +1,56 @@
-// Saves options to localStorage.
 function save_options() {
+
   var select = document.getElementsByName("font_name");
   localStorage["fontname"] = select.item(0).value;
-
-  // Update status to let user know options were saved.
   var status = document.getElementById("status");
-  status.innerHTML = "Options Saved.";
+  status.innerHTML = "Options Saved. Refresh the page to see the change.";
   setTimeout(function() {
     status.innerHTML = "";
-  }, 750);
+  }, 1500);
+
 }
 
-// Restores select box state to saved value from localStorage.
 function restore_options() {
+
   var favorite = localStorage["fontname"];
   if (!favorite) {
     return;
   }
-  var select = document.getElementsByName("font_name");
-  select.item(0).value = favorite;
-  /*
-  var select = document.getElementById("color");
-  for (var i = 0; i < select.children.length; i++) {
-    var child = select.children[i];
-    if (child.value == favorite) {
-      child.selected = "true";
-      break;
-    }
+  
+  var input = document.getElementById("font_name");
+  if (input) {
+    input.value = favorite;
   }
-  */
+
 }
 
 chrome.extension.onRequest.addListener(
 function(request, sender, sendResponse) {
-if (request.localstorage == "fontname")
-	sendResponse({fontname: localStorage["fontname"]});
-else
-	sendResponse({});
+
+  if (request.localstorage == "fontname")
+    sendResponse({fontname: localStorage["fontname"]});
+  else
+    sendResponse({});
+
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById("click-this").addEventListener("click", save_options);
+  var link = document.getElementById("click-this");
+  if (link != undefined) {
+    link.addEventListener('click', function() {save_options();});
+  }
+  // document.getElementById("click-this").addEventListener("click", save_options);
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-    var link = document.getElementById("click-this");
-    // onClick's logic below:
-    link.addEventListener('click', function() {
-        save_options();
-    });
+
+  var link = document.getElementById("click-this");
+  if (link != undefined) {
+    link.addEventListener('click', function() {save_options;});
+  }
+
 });
 
 document.addEventListener('DOMContentLoaded', function() {
   restore_options();
-  
 });
