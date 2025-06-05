19f2afabb792c7750c8500e293d8f25983cefce4
whats-g00d <sgoodman92@gmail.com>
2025-06-05 06:28:41 -0500

    Update foo.js

--- foo.js
+++ foo.js
@@ -5,7 +5,7 @@ chrome.extension.sendRequest({localstorage: "fontname"}, function(response) {
 	
 	fontname=response.fontname;
 	var divNode = document.createElement("div");
-	divNode.innerHTML = '<style>*{font-family:' + fontname + ',sans-serif!important;}</style>';
+	divNode.innerHTML = '<style>*{font-family:' + fontname + ',sans-serif!important; font-weight: bold!important;}</style>';
 	var head = document.getElementsByTagName('head')[0];
 	head.appendChild(divNode);
 	
